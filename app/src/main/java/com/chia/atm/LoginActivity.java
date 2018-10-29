abbaa72106473f38404feda2fcec757405fe04cb
cac0709 <cac91053@gmail.com>
2018-10-29 20:27:05 +0800

    LOGIN CANCEL CREATE

--- app/src/main/java/com/chia/atm/LoginActivity.java
+++ app/src/main/java/com/chia/atm/LoginActivity.java
@@ -4,21 +4,36 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+    private EditText ed_userid;
+    private EditText ed_passwd;
+    private String username;
+    private String password;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
-
     }
 
-    public void login (View view){
+
+    public void login(View view) {
         Log.d("LoginActivity", "login");
-     
+        ed_userid = findViewById(R.id.ed_userid);
+        ed_passwd = findViewById(R.id.ed_passwd);
+        username = ed_userid.getText().toString();
+        password = ed_passwd.getText().toString();
+        if ("yuchia".equals(username) && "000000".equals(password)) {
+            finish();
 
-
+        }
+        }
+    public void cancel(View view){
+        Log.d("LoginActivity","cancel");
     }
+
 }
+
