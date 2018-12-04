a4685a709ca78df08709b1b817bb0469d0a9f36d
cac0709 <cac91053@gmail.com>
2018-12-04 16:27:35 +0800

    JUST ADDAPTER 12/4

--- app/src/main/java/com/chia/atm/AgeActivity.java
+++ app/src/main/java/com/chia/atm/AgeActivity.java
@@ -1,18 +1,32 @@
 package com.chia.atm;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class AgeActivity extends AppCompatActivity {
-
+    int[] numbers = {20,21,22,23,24,25};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_age);
+        RecyclerView recyclerView = findViewById(R.id.recycler2);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        //TODO:SET ADAPTER
+        recyclerView.setAdapter(new AgeAdapter());
+
     }
+
     public void next (View view){
         EditText AGE = findViewById(R.id.age);
          float userage = Float.parseFloat(AGE.getText().toString());
@@ -26,6 +40,43 @@ public class AgeActivity extends AppCompatActivity {
     public void back(View view) {
         finish();
    }
+    class AgeAdapter extends RecyclerView.Adapter<AgeAdapter.AgeViewHolder> {
+
+        @NonNull
+        @Override
+        public AgeViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View view = getLayoutInflater().inflate(R.layout.age_row,parent,false);
+            return new AgeViewHolder(view) ;
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull AgeViewHolder holder, int position) {
+            holder.ageText.setText(numbers[position]+"");
+            if (numbers[position] == 19 ){
+                holder.ageText.setTextColor(Color.RED);
+
+            }
+
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return numbers.length;
+
+        }
+
+        class AgeViewHolder extends RecyclerView.ViewHolder {
+            TextView ageText;
+            public AgeViewHolder(View itemView) {
+                super(itemView);
+                ageText = itemView.findViewById(R.id.tv_age);
+
+
+            }
+        }
+    }
 }
+
 
 
--- app/src/main/java/com/chia/atm/MainActivity.java
+++ app/src/main/java/com/chia/atm/MainActivity.java
@@ -1,18 +1,19 @@
 package com.chia.atm;
 
 import android.content.Intent;
-import android.provider.ContactsContract;
-import android.support.v7.app.AlertController;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 
-import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -20,7 +21,7 @@ public class MainActivity extends AppCompatActivity {
     private static final int RC_LOGIN = 100;
     boolean logon = false;
 
-
+    List<String> fruits = Arrays.asList("香蕉", "鳯梨", "芭樂");
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,17 +29,44 @@ public class MainActivity extends AppCompatActivity {
         if (!logon) {
             Intent intent = new Intent(this, LoginActivity.class);
             startActivityForResult(intent, RC_LOGIN);
-            RecyclerView recyclerView = findViewById(R.id.recycler);
-            recyclerView.setHasFixedSize(true);
-            recyclerView.setLayoutManager(new LinearLayoutManager(this));
-            recyclerView.setAdapter(new FruitAdapter());
         }
         //listView();
+        //ListView
+        //RecyclerView
+        RecyclerView recyclerView = findViewById(R.id.recycler);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(new FruitAdapter());
     }
+
     class FruitAdapter extends RecyclerView.Adapter<FruitAdapter.FruitViewHolder>{
 
-    }
+        @NonNull
+        @Override
+        public FruitViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext())
+                    .inflate(android.R.layout.simple_list_item_1, parent, false);
+            return new FruitViewHolder(view);
+        }
 
+        @Override
+        public void onBindViewHolder(@NonNull FruitViewHolder holder, int position) {
+            holder.nameText.setText(fruits.get(position));
+        }
+
+        @Override
+        public int getItemCount() {
+            return fruits.size();
+        }
+
+        class FruitViewHolder extends RecyclerView.ViewHolder {
+            TextView nameText;
+            public FruitViewHolder(View itemView) {
+                super(itemView);
+                nameText = itemView.findViewById(android.R.id.text1);
+            }
+        }
+    }
 
 
         private void listView() {
